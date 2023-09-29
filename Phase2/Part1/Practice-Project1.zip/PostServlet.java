1253f24c04a25f4b5e10aefb9af8440059d9724d
dineshkumar-1234 <144683634+dineshkumar-1234@users.noreply.github.com>
2023-09-29 18:30:23 +0530

    Create PostServlet.java

---
+++ Phase2/Part1/Practice-Project1.zip/PostServlet.java
@@ -0,0 +1,24 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PostServlet
+ */
+public class PostServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String paramValue = request.getParameter("param");
+		response.getWriter().write("POST Request Parameter: " + paramValue);
+	}
+
+}
