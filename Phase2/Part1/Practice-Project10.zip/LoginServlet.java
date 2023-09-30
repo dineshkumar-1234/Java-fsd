816f7f1c29a24810b2755aaff325a0104806a4af
dineshkumar-1234 <144683634+dineshkumar-1234@users.noreply.github.com>
2023-09-30 17:12:31 +0530

    Create LoginServlet.java

---
+++ Phase2/Part1/Practice-Project10.zip/LoginServlet.java
@@ -0,0 +1,24 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().invalidate();
+		response.sendRedirect("Login.html");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
