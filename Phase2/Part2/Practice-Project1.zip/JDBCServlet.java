765e9673b83f43789fa366954e05417f2f54c4a6
dineshkumar-1234 <144683634+dineshkumar-1234@users.noreply.github.com>
2023-10-04 11:49:19 +0530

    Create JDBCServlet.java

---
+++ Phase2/Part2/Practice-Project1.zip/JDBCServlet.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet
+
+public class JDBCServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String jdbcUrl = "jdbc:mysql://localhost:3306/ecommerce";
+			String username = "root";
+			String password = "q1w2e3r4t5y6u7!";
+			connection = DriverManager.getConnection(jdbcUrl, username, password);
+			out.println("JDBC environment set up successfully!");
+			}
+		catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			out.println("Error: " + e.getMessage());
+			}
+		finally {
+			try {
+				if (connection != null && !connection.isClosed()) {
+					connection.close();
+					}
+				}
+			catch (SQLException e) {
+				e.printStackTrace();
+				}
+			}
+		}
+}
