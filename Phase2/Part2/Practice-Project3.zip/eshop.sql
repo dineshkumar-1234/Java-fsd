1c421deca2346efc14d4cb479820707c8d3f8273
dineshkumar-1234 <144683634+dineshkumar-1234@users.noreply.github.com>
2023-10-04 14:33:08 +0530

    Create eshop.sql

---
+++ Phase2/Part2/Practice-Project3.zip/eshop.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE eshop;
+
+USE eshop;
+
+CREATE TABLE product (
+product_id INT AUTO_INCREMENT PRIMARY KEY,
+product_name VARCHAR(255) NOT NULL,
+product_description TEXT,
+product_price Double(20,2) NOT NULL,
+product_quantity INT NOT NULL DEFAULT 0
+);
+
+INSERT INTO product (product_name, product_description, product_price, product_quantity)
+VALUES
+('Laptop', 'High-performance laptop with SSD storage', 54000, 10),
+('Smartphone', 'Latest smartphone with advanced features', 25000, 15),
+('Headphones', 'Wireless noise-canceling headphones', 250, 20),
+('Tablet', 'Portable tablet with long battery life', 10000, 12),
+('Camera', 'Digital camera with high-resolution sensor', 54000, 8);
