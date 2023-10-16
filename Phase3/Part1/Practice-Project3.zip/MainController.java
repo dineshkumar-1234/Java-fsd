2066f7cca97e2e78f1e33104d3d7bec649cb8d21
dineshkumar-1234 <144683634+dineshkumar-1234@users.noreply.github.com>
2023-10-16 20:13:53 +0530

    Create MainController.java

---
+++ Phase3/Part1/Practice-Project3.zip/MainController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ecommerce.dao.EProductDAO;
+import com.ecommerce.entity.EProduct;
+
+@Controller
+public class MainController {
+  @Autowired 
+  EProductDAO eproductDAO;    
+  @RequestMapping(value = "/listProducts", method = RequestMethod.GET)
+  public String listProducts(ModelMap map)
+  {
+      List<EProduct> list= eproductDAO.getProducts();
+      List.addAttribute("list",list);  
+      return "listProducts";
+    }
+}
